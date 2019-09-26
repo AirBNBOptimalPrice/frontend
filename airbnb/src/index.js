2acfd9f475778ca1c07974530ef0b3251e2b6489
Surfsol <surfsol@protonamil.com>
2019-09-26 13:07:32 -0300

    updated Landing.html, index.css

--- airbnb/src/index.js
+++ airbnb/src/index.js
@@ -5,4 +5,4 @@ import App from './App';
 import { BrowserRouter as Router } from "react-router-dom"; //Routes will have props/state objects! history and match
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+ReactDOM.render(<Router><App/></Router>, document.getElementById('root'));
